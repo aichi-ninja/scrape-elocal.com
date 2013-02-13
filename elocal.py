1f91ac9fd5e86da9afdfa2652e5c20653e6c1673
sadmoon <sadmoon@hush.ai>
2013-02-12 16:56:42 -0800

    brokens.

--- elocal.py
+++ elocal.py
@@ -35,3 +35,21 @@ def getCitiesByStateLink(url):
             for li_a in li:
                 city_links.append(li_a.find('a').get('href'))
     return city_links
+
+#x. check if we have a next link
+def getNextLink(url):
+    html = BeautifulSoup(getHtml(url))
+    return html.find('ul','page-list').find('a','arrow-right').get('href')
+
+#x. get all the links to the business profiles
+url = "http://www.elocalplumbers.com/city/Anchorage_AK/#!/page=2"
+#html = BeautifulSoup(getHtml(url))
+#listings = html.find_all('article','listing')
+#for listing in listings:
+#    bus_phone = listing.find('li','view-phone').get_text()
+#    name = listing.find('h2')
+#    bus_name = name.find('a').get_text()
+    
+print str(getNextLink(url))
+    
+#x. scrape business information
---
+++ readme.md
@@ -0,0 +1,8 @@
+what can this file do?
+-get the html of a url
+-get all state links on elocal
+-get all city links by state from elocal
+-archive business information from elocal
+
+keep in mind
+-elocal shows 3 results per page
