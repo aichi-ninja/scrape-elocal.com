49469ba0c52f93e7d5f71e88b9a116f2967a548f
sadmoon <sadmoon@hush.ai>
2013-02-12 16:21:13 -0800

    get cities from state links

--- elocal.py
+++ elocal.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
+#aichi-ninja 2013
 import urllib2
 from bs4 import BeautifulSoup
 
+#get html from a link
 def getHtml(url):
     sock = urllib2.urlopen(url)
     data = sock.read()
     sock.close()
     return data
 
+#get state links from the main page
 def getStateLinks():
     state_links = []
     url = "http://www.elocalplumbers.com/"
@@ -17,3 +21,17 @@ def getStateLinks():
         for link in links:
             state_links.append("http://www.elocalplumbers.com" + link.find('a').get('href'))
     return state_links
+
+#get city links by state
+#ex; http://www.elocalplumbers.com/ak
+def getCitiesByStateLink(url):
+    city_links = []
+    html = BeautifulSoup(getHtml(url))
+    cities = html.find_all('div','blue-fill')
+    for city in cities:
+        ul = city.find_all('ul')
+        for u in ul:
+            li = u.find_all('li')
+            for li_a in li:
+                city_links.append(li_a.find('a').get('href'))
+    return city_links
